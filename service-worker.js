337dfa03bc1bd945f7011491c6acd9b56f6c1d91
evanworth <Evan.worth@gmail.com>
2026-01-21 14:19:14 -0500

    v3.3

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,2 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('nms-period-tracker-v2').then(cache => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('nextblock-v3').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
